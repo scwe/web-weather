e1102b5bded4eac4848fbde3b06228a144b21225
Scott <weston.scot@gmail.com>
2016-09-10 17:37:58 +0200

    Tidied up routes and added a 404 file

--- app.js
+++ app.js
@@ -15,35 +15,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 //We do this, otherwise there is some weird caching that breaks and returns a 304
 app.disable('etag');
 
-app.use('/api', api);
 
-app.use('/', function(req, res, next){
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+app.get('/', function(req, res, next){
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
 //================
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.send('error'+ err.message );
-  });
+if (app.get('env') !== 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.send('error'+ err.message );
+    });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.send('error'+ err.message );
+    res.status(err.status || 500);
+    res.sendFile(path.join(__dirname, "public", "404.html"));
 });
 
 
--- routes/api.js
+++ routes/api.js
@@ -9,14 +9,16 @@ var router = express.Router();
 var cityData = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data', 'cities_new.json')));
 var citySearch = new Fuse(cityData.cities);
 
+//The query in this function works regardless of whether you use
+//A zip code or a name of the city
 router.get('/weather', function(req, res, next){
     var names = req.query.names.split(',');
     if(names.length === 0){
         return res.json({});
     }
-    
+
     var queryString = "SELECT * FROM weather.forecast WHERE woeid IN (SELECT woeid FROM geo.places(1) WHERE ";
-    queryString += _.map(names, function(n){return "text=\""+n+"\"";}).join(" OR ") + ")";
+    queryString += _.map(names, function(n){return "text=\""+n+"\"";}).join(" OR ") + ") and u=\"c\"";
 
     var query = new YQL(queryString);
     query.exec(function(error, response){
@@ -25,7 +27,11 @@ router.get('/weather', function(req, res, next){
 });
 
 router.get('/weather/:name', function(req, res, next){
-    var query = new YQL("SELECT * FROM weather.forecast WHERE woeid IN (SELECT woeid FROM geo.places(1) WHERE text=\""+req.params.name+"\")");
+    var query = new YQL(
+        "SELECT * FROM weather.forecast WHERE woeid IN "+
+        "(SELECT woeid FROM geo.places(1) WHERE text=\""+req.params.name+"\")"+
+        " and u=\"c\"");
+    
     query.exec(function(error, response){
         res.json(response.query.results);
     });
@@ -36,7 +42,10 @@ router.get('/cities', function(req, res, next){
 });
 
 router.get('/cities/:subname', function(req, res, next){
-    var searchResults = _.map(citySearch.search(req.params.subname), function(index){return cityData.cities[index];});
+    var searchResults = _.map(citySearch.search(req.params.subname), function(index){
+            return cityData.cities[index];
+        });
+
     res.json(_.take(searchResults, req.query.max ? req.query.max : 10));
 });
 
