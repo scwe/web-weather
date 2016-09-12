6ca36f31fed74860fcf1ec970a3ca881bf445cdd
Scott <weston.scot@gmail.com>
2016-09-13 00:36:36 +0200

    Change webpack to use uglifyjs and NODE_ENV production

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+
+var PROD = (process.env.NODE_ENV === 'production')
+
+var plugins = [
+    new webpack.DefinePlugin({
+        'process.env': {
+            'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        }
+    })
+]
+
+if(PROD){
+    plugins.push(new webpack.optimize.UglifyJsPlugin({
+        compress: { warnings: false }
+    }));
+}
+
 module.exports = {
     entry: "./public/js/app.js",
     output: {
         path: __dirname,
         filename: "./public/bundle.js"
     },
+    plugins: plugins,
     module: {
         loaders: [
             {test : /\.css$/, loader: "style!css"},
